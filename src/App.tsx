e5028fc8bb73a0f4694393619fbea4467b55879a
temcha <itemchur@spin.ai>
2025-01-31 21:55:21 +0200

    added tailwind classes sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="text-3xl font-bold underline">React Vite Ziplock</p>
-    </>
-  );
+  return <p className="text-3xl font-bold underline">React Vite Ziplock</p>;
 }
 
 export default App;
